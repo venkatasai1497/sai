081428afda04122421bc88b667fe95095aee9a97
Ubuntu <ubuntu@ip-172-31-86-79.ec2.internal>
2022-03-10 07:57:28 +0000

    script.sh

--- script.sh
+++ script.sh
@@ -13,6 +13,7 @@ if [ -s file.txt ]
 then  rm file.txt
       git add .
       git commit -am "commit the changed.py file"
+      git push -u origin master
       git push "https://venkatasai1497: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@github.com/venkatasai1497/sai.git"
 else echo "no chabges done"
 fi
