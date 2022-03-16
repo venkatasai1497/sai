5a65d9d3d536255c9891b3a42c410cb8c2e84090
Ubuntu <ubuntu@ip-172-31-95-10.ec2.internal>
2022-03-16 07:28:39 +0000

    updated script

---
+++ script2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#apt-get install python-autopep8
+find . -name '*.py' > file.txt
+#cat file.txt
+while read -r line
+do autopep8 --in-place --aggressive --aggressive "$line"
+done < file.txt
+ 
+git status | grep ".py$" > file.txt
+cat file.txt
+if [ -s file.txt ]
+then   rm file.txt
+	git add .
+        git commit -am "Commit done"
+        git config user.email "venkata.sai1497@gmail.com"
+        git config user.name "venkatasai1497"
+        git push "https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/venkatasai1497/gitchanges.git"
+else  echo "no changes done"
+fi
