b9b5564d5c3c6b26b2cb11e03dbf7e2113a4a7fd
Ubuntu <ubuntu@ip-172-31-83-120.ec2.internal>
2022-03-17 11:54:19 +0000

    maven.sh

--- maven.sh
+++ maven.sh
@@ -3,8 +3,8 @@ mvn -version > file.txt
 cat file.txt
 if [ -s file.txt ]
 then echo "maven instaleed"
-else sudo apt-get update
-     sudo apt-get install maven -y
+else  apt-get update
+      apt-get install maven -y
      mvn --version
 fi
 
