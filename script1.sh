c084bb88c6bf78d683c01a0be9365b6d8c4b8fdf
Ubuntu <ubuntu@ip-172-31-81-169.ec2.internal>
2022-03-17 07:13:32 +0000

    script1.sh

--- script1.sh
+++ script1.sh
@@ -6,14 +6,12 @@ do autopep8 --in-place --aggressive --aggressive "$line"
 done < file.txt
 
 
-git add .
 git status | grep ".py$" > file.txt
 cat file.txt
 if [ -s file.txt ]
 then  rm file.txt
       git add .
       git commit -am "commit the changed.py file"
-      git push -u origin master
       git push "https://venkatasai1497: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@github.com/venkatasai1497/sai.git"
 else echo "no chabges done"
 fi
