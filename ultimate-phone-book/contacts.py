a160aab5306c1fe88adf6fabb65ed88757491621
Ubuntu <ubuntu@ip-172-31-87-195.ec2.internal>
2022-03-23 07:17:41 +0000

    updated

--- ultimate-phone-book/contacts.py
+++ ultimate-phone-book/contacts.py
@@ -22,7 +22,9 @@ infile.close()
 # get key if path exists
 
 
-keyacess = False
+
+
+keyacess =   False
 path = 'data/pickle-key'
 if os.path.isfile('data/pickle-key'):
     pklekey = open('data/pickle-key', 'rb')
